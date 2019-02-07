ed38b9a854367ff691109e77a6176e1347d664e8
Vincent BESANCON <besancon.vincent@gmail.com>
2019-02-07 23:30:29 +0100

    Remove HTTPS self-signed certs
    Use a reverse proxy instead.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,5 @@ PATH="/bin:/sbin:/usr/bin:/usr/sbin"
 php /configure-db.php || exit 1
 su nginx -s /bin/sh -c "php /srv/ttrss/update.php --update-schema" || exit 1
 
-# Generate SSL self-signed certs
-if [ ! -e "/etc/nginx/ssl/ttrss.crt" ]
-then
-  openssl req \
-    -subj "/CN=${TTRSS_SELF_CERT_CN}/O=${TTRSS_SELF_CERT_ORG}/C=${TTRSS_SELF_CERT_COUNTRY}" \
-    -new \
-    -newkey rsa:2048 \
-    -days 1825 \
-    -nodes \
-    -x509 \
-    -keyout /etc/nginx/ssl/ttrss.key \
-    -out /etc/nginx/ssl/ttrss.crt
-fi
-
 # Relay to supervisord
 exec supervisord -c /supervisord.conf
