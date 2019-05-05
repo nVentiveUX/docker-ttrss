2540462d336398080207fe1fd1e7861d91c24eed
Vincent BESANCON <besancon.vincent@gmail.com>
2019-05-05 16:53:24 +0200

    emails: fix msmtprc perms and ownership

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ php /configure-db.php || exit 1
 su nginx -s /bin/sh -c "php /srv/ttrss/update.php --update-schema" || exit 1
 
 # Configure mail sending
+touch /etc/msmtprc
+chown nginx:nginx /etc/msmtprc
+chmod 0400 /etc/msmtprc
 envsubst < /var/tmp/msmtprc.tpl > /etc/msmtprc
 
 # Relay to supervisord
