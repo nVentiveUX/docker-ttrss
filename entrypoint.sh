cd7a7fd0340bc28ce0e9e9cc6d61ae08a8845afd
Vincent BESANCON <besancon.vincent@gmail.com>
2021-03-08 23:26:38 +0100

    feat!: migrate to global config
    https://tt-rss.org/wiki/GlobalConfig

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@ set -e
 
 PATH="/bin:/sbin:/usr/bin:/usr/sbin"
 
-# Configure TT-RSS database and schema migrations
-php /configure-db.php || exit 1
-su nginx -s /bin/sh -c "php /srv/ttrss/update.php --update-schema" || exit 1
+su nginx -s /bin/sh -c "php /srv/ttrss/update.php --update-schema=force-yes" || exit 1
 
 # Configure mail sending
 touch /etc/msmtprc
